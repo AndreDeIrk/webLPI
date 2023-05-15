a4df0d70485e425d8fa7f331e9c8b6c1a91b9416
andreyB <megapolisirk.andre@gmail.com>
2023-05-15 11:32:04 +0300

    big update

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -7,6 +7,10 @@ import Login from './components/Login';
 import useToken from './components/useToken';
 import HeaderSwitcher from "./components/elements/HeaderSwitcher";
 
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
 function App(props) {
     const { token, setToken } = useToken();
     const [ page, setPage ] = useState(window.location.pathname);
@@ -15,26 +19,44 @@ function App(props) {
         return <Login setToken={setToken}/>
     }
 
+    const tabs = [
+        {href: '/', name: 'Home'},
+        {href: '/settings', name: 'Settings'},
+        {href: '/about', name: 'About'},
+        {href: '/login', name: 'Login'},
+    ]
+
     return (
-        <div className={"body text " + props.className}>
-                <div className={"header-bar"}>
-                    <Link to="/" onClick={() => {setPage('/')}}>
-                        <button className={"header-btn" + (page === '/' ? " active" : "")}>
-                            Home
-                        </button>
-                    </Link>
-                    <HeaderSwitcher to={'Settings'} onClick={setPage} page={page}/>
-                    <HeaderSwitcher to={'About'} onClick={setPage} page={page}/>
-                </div>
-                <div className={'content'}>
-                    <Routes>
-                        <Route path="/" element={<Home />}/>
-                        {/*<Route path="/login" element={<Login setToken={setToken}/>}/>*/}
-                        <Route path="/settings" element={<Settings />}/>
-                        <Route path="/about" element={<About />}/>
-                    </Routes>
-                </div>
+        <div className={`body ${props.className}`}>
+            <Navbar sticky='top' bg='dark' variant="dark" expand='sm'>
+                <Container>
+                    <Navbar.Brand>Navbar</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className='me-auto'>
+                        {tabs.map((tab) => 
+                            <Nav.Link as={Link} to={tab.href} onClick={() => {setPage(tab.href)}}>
+                                {tab.name}
+                            </Nav.Link>
+                        )}
+                            
+                            {/* <HeaderSwitcher to={'Settings'} onClick={setPage} page={page}/>
+                            <HeaderSwitcher to={'About'} onClick={setPage} page={page}/>                    
+                            <HeaderSwitcher to={'Login'} onClick={setPage} page={page}/> */}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <div className={'content'}>
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                    {/*<Route path="/login" element={<Login setToken={setToken}/>}/>*/}
+                    <Route path="/settings" element={<Settings />}/>
+                    <Route path="/about" element={<About />}/>
+                    <Route path="/login" element={<Login setToken={setToken}/>}/>
+                </Routes>
             </div>
+        </div>
     );
 }
 
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,130 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import './styles/global.scss';
-import Button from "./elements/Buttons";
+// import Button from "./elements/Buttons";
 import Request from "./Request";
-// import Gear from "../imgs/gear.svg"
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isSignIn: false,
-            isInputTg: true,
-            loginState: 'login',
-            username: "-",
-            password: "-",
-        }
-        this.switchForm = this.switchForm.bind(this);
-        this.submitForm = this.submitForm.bind(this);
-    }
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-    async switchForm(event) {
+const Login = (props) => {
+    const [ SI, setSI ] = useState(true);
+    const [ SUcode, setSUcode ] = useState(false);
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    async function switchForm(event, state) {
         event.preventDefault();
-        this.setState({isSignIn: !this.state.isSignIn, isInputTg: true});
+        setSI(state);
+        setSUcode(false);
     }
 
-    async loginUser(credentials) {
+    async function loginUser(credentials) {
         return await Request('/auth', JSON.stringify(credentials));
     }
 
-    async submitForm(event) {
+    async function submitForm(event) {
         event.preventDefault();
 
-        if (this.state.username === "-" || this.state.password === "-") {
-            this.setState({
-                username: (this.state.username === "-" ? "" : this.state.username),
-                password: (this.state.password === "-" ? "" : this.state.password)
-            });
+        const token = await loginUser({
+            telegram: username,
+            password: password
+        })
+        props.setToken(token.status ? token : {
+            token: ""
+        });
+        if (!token.status) {
+            console.log(token.detail);
         }
-        else {
-            const token = await this.loginUser({
-                telegram: this.state.username,
-                password: this.state.password
-            })
-            this.props.setToken(token.status ? token : {
-                token: ""
-            });
-            if (!token.status) {
-                console.log(token.detail);
-            }
-        }
+        
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        // console.log(this.state.username);
-    }
-
-    render() {
-        return (
-            <div className={'body ' + this.props.className} data-center>
-                <div className={"switch-container dark"}>
-                    <form data-hide-left={!this.state.isSignIn}
-                          style={{width: '300px'}}
-                          data-align="center">
-                        <h1>Sign In</h1>
-                        <input name="username" type="text" placeholder={'Telegram'}
-                               onChange={event => this.setState({username: event.target.value})}
-                               data-large/>
-                        <input name="password" type="password" placeholder={'Password'}
-                               onChange={event => this.setState({password: event.target.value})}
-                               data-large/>
-                        <div className="row">
-                            <div className="col-3"></div>
-                            <Button size={'47px'} type={'submit'} state={this.state.loginState}
-                                    className='col-6'
+    
+    return (
+        <div className={`body ${props.className}`} data-center>
+            <div className={"switch-container dark"}>
+                <Form data-hide-left={!SI}
+                        className="p-3">
+                    <h1>Sign In</h1>
+                    <Form.Control name="username" type="text"
+                                    className="mb-3 web-input" 
+                                    placeholder={'Telegram'}
+                                    onChange={event => setUsername(event.target.value)}/>
+                    <Form.Control name="password" type="password" 
+                                    className="mb-3 web-input" 
+                                    placeholder={'Password'}
+                                    onChange={event => setPassword(event.target.value)}/>
+                    <Row>
+                        <Col>
+                            <button type="submit"
+                                    className='web-btn'
                                     onClick={(e) => {
-                                        this.submitForm(e);
-                                    }}/>
-                            <button className={'btn-link col-3'}
+                                        submitForm(e);
+                                    }}>
+                                Submit
+                            </button> 
+                        </Col>
+                        <Col>
+                            <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
+                                <button type="link"
+                                        className='web-btn'
+                                        onClick={(e) => {
+                                            switchForm(e, false);
+                                        }}>
+                                    Sign Up
+                                </button> 
+                            </div>
+                        </Col>
+                    </Row>
+                </Form>
+
+                <Form data-hide-right={SI}
+                        className='p-3'>
+                    <h1 className="form-title">Sign Up</h1>
+                    <Form.Control name="newUsername"
+                                    className="web-input mb-3"
+                                    type="text"
+                                    placeholder={'Telegram'}
+                                    data-last/>
+                    <Form.Control name="code"
+                                    className="web-input mb-3"
+                                    type="text"
+                                    value={'0 1 2 3'}
+                                    disabled hidden={!SUcode}/>
+                    <Row>
+                        <Col>
+                            <button type='link'
                                     onClick={(e) => {
-                                        this.switchForm(e);
-                                    }}>Sign Up</button>
-                        </div>
-                    </form>
-
-                    <form data-hide-right={this.state.isSignIn}
-                          style={{width: '300px'}}
-                          data-align="center">
-                        <h1 className="form-title">Sign Up</h1>
-                        <div className="input-group switch-container" style={{width: '100%'}}>
-                            <input name="newUsername"
-                                   id="tg-input-new"
-                                   type="text"
-                                   data-hide-top={!this.state.isInputTg}
-                                   data-align='center'
-                                   data-large
-                                   placeholder={'Telegram'}
-                                   data-last/>
-                            <input id="code"
-                                   name="code"
-                                   type="text"
-                                   value={'0 1 2 3'}
-                                   data-hide-bottom={this.state.isInputTg}
-                                   data-align='center'
-                                   data-large
-                                   data-last
-                                   disabled/>
-                        </div>
-                        <button className={'btn-link'}
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    this.setState({isInputTg: !this.state.isInputTg});
-                                }}>Get code</button>
-                        <button className={'btn-link mt-5'}
-                                onClick={(e) => {
-                                    this.switchForm(e);
-                                }}>Sign In</button>
-                    </form>
-                </div>
-
-                {/*<img src={Gear}*/}
-                {/*     style={{width: '25%'}}*/}
-                {/*     className={'spinner-img'}/>*/}
-
+                                        e.preventDefault();
+                                        setSUcode(!SUcode);
+                                    }}
+                                    className={'web-btn'}>Get code</button>
+                        </Col>
+                        <Col>
+                            <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
+                                <button type='link'
+                                        className='web-btn'
+                                        onClick={(e) => {
+                                            switchForm(e, true);
+                                        }}>Sign In</button>
+                            </div>
+                        </Col>
+                    </Row>
+                </Form>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 Login.propTypes = {
